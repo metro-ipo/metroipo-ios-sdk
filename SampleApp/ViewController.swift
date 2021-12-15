26a16ffba2e7d941a2ff951bafe053cc7578d784
Luechen Christopher <luechenchris@gmail.com>
2021-12-15 09:44:09 -0500

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Luechen Christopher on 12/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
