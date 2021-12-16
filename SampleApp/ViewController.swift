a422c50897077cbc22319a37b127c43c6674f26f
Luechen Christopher <luechenchris@gmail.com>
2021-12-15 19:34:04 -0500

    fix: added metro ipo sdk pod

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import MetroIpoSdk
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var messageLabel: UILabel!
+  @IBOutlet weak var codeTextField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    messageLabel.text = "To add your signature, please enter your verification code. To upload your signature, after signing on the next screen, press the NEXT button."
   }
-
-
+  
+  @IBAction func onSubmitPress(_ sender: Any) {
+    guard let verificationCode = codeTextField.text else {
+        return
+    }
+    do {
+      let config = MetroIpoConfig()
+        .setCode(code: verificationCode)
+        .setDomain(url: "https://admin.metroipo.com")
+        .build()
+      var presentationStyle: UIModalPresentationStyle = .fullScreen
+      
+      if UIDevice.current.userInterfaceIdiom == .pad {
+        presentationStyle = .formSheet
+      }
+      let sdk = try MetroIpo(configuration: config)
+      try sdk.start(origin: self, style: presentationStyle)
+      sdk.with(responseHandler: {response in
+        switch response {
+        case .success(let result):
+          self.showAlert(title: "Congratulations", message: result)
+        case .failure(let error):
+          print("Metro IPO Sdk encounterd an error: \(error.localizedDescription)")
+        case .error(let error):
+          switch error {
+          case .exception(withMessage: let message):
+            self.showAlert(title: "Invalid Code", message: message)
+          case .USER_CANCELLED:
+            print("Applicant cancelled verification.")
+          case .API_NOT_AVAILABLE:
+            print("Server is offline.")
+          case .UPLOAD_INVALID:
+            print("Upload data is corrupted.")
+          case .CODE_MISSING:
+            print("Verification code is missing.")
+          default:
+            print("An error occured. \(error.localizedDescription)")
+          }
+        case .start:
+          print("Metro IPO Sdk was initialized")
+        @unknown default:
+          print("Default response")
+        }
+      })
+    } catch let error {
+      print("Flow not started. Error: \(error)")
+    }
+  }
+  
+  private func showAlert(title: String, message: String) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alertController.addAction(UIAlertAction(title: "Ok",
+                                            style: .default,
+                                            handler: nil))
+    self.present(alertController, animated: true, completion: nil)
+  }
 }
 
